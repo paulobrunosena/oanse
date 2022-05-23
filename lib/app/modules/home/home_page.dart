66a85f214f3d513d24b670406e76783b7e213404
Paulo Bruno Batista de Sena <paulo.sena@prodam.am.gov.br>
2022-05-23 08:00:53 -0400

    atualização para flutter 3

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = "Home"}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final store = Modular.get<HomeStore>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Observer(
+        builder: (context) => Text('${store.counter}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
