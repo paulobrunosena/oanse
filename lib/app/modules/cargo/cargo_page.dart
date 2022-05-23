69097380260975f7cc6028d64986d3eedf15a6d9
Paulo Bruno Batista de Sena <paulo.sena@prodam.am.gov.br>
2022-05-23 15:26:56 -0400

    alterada tela para cargo novamente

--- lib/app/modules/cargo/cargo_page.dart
+++ lib/app/modules/cargo/cargo_page.dart
@@ -17,9 +17,59 @@ class CargoPageState extends State<CargoPage> {
 
   @override
   void initState() {
-    store.list();
+    //store.list();
+    store.listUsers();
     super.initState();
   }
+  //Tela exemplo de usuários pegando os dados de uma api em produção
+  /*@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Observer(
+            builder: (_) {
+              if (store.loadingList) {
+                return Container();
+              } else {
+                if (store.listUser.isEmpty) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: const Text("Nenhum cargo cadastrado"),
+                  );
+                } else {
+                  return ListView.separated(
+                    itemCount: store.listUser.length,
+                    itemBuilder: (_, index) {
+                      User item = store.listUser[index];
+                      return ListTile(
+                        title: Text(item.name ?? "Nome de usuário"),
+                        subtitle: Text("Descrição: ${item.email}"),
+                        trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                      );
+                    },
+                    separatorBuilder: (_, __) {
+                      return const Divider();
+                    },
+                  );
+                }
+              }
+            },
+          ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Adicionar',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }*/
+
+  //Tela de cargos que não está funcionando no flutter web, pois precisa ativar o CORS no header do servidor da API
 
   @override
   Widget build(BuildContext context) {
--- lib/app/modules/cargo/cargo_store.dart
+++ lib/app/modules/cargo/cargo_store.dart
@@ -2,6 +2,7 @@ import 'package:asuka/asuka.dart' as asuka;
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:mobx/mobx.dart';
+import 'package:oanse/app/modules/cargo/model/user.dart';
 
 import 'model/cargo.dart';
 import 'services/interfaces/cargo_service_interface.dart';
@@ -23,6 +24,8 @@ abstract class CargoStoreBase with Store {
 
   ObservableList<Cargo> listCargo = ObservableList<Cargo>();
 
+  ObservableList<User> listUser = ObservableList<User>();
+
   Future<void> list() async {
     loadingList = true;
     listCargo.clear();
@@ -39,6 +42,28 @@ abstract class CargoStoreBase with Store {
           duration: const Duration(seconds: 3)));
     }, (list) {
       listCargo.addAll(list ?? []);
+      EasyLoading.dismiss();
+    });
+
+    loadingList = false;
+  }
+
+  Future<void> listUsers() async {
+    loadingList = true;
+    listUser.clear();
+    EasyLoading.show(status: "Carregando dados, aguarde...");
+
+    isInitial = false;
+    var result = await _service.listUser();
+
+    result.fold((failure) {
+      EasyLoading.dismiss();
+      asuka.showSnackBar(SnackBar(
+          content: Text(failure.message!),
+          backgroundColor: Colors.redAccent,
+          duration: const Duration(seconds: 3)));
+    }, (list) {
+      listUser.addAll(list ?? []);
       EasyLoading.dismiss();
     });
 
