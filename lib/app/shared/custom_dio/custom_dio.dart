c0ace2e05563a86d84d9ce47dfedbe0d66933128
Paulo Sena <paulo.sena@prodam.am.gov.br>
2022-05-23 20:39:34 -0400

    correção de httpcliente na web

--- lib/app/shared/custom_dio/custom_dio.dart
+++ lib/app/shared/custom_dio/custom_dio.dart
@@ -1,10 +1,15 @@
+import 'package:dio/adapter_browser.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
+import 'package:flutter/foundation.dart';
 
 import 'interceptors.dart';
 
 class CustomDio extends DioForNative {
   CustomDio([BaseOptions? options]) : super(options) {
     interceptors.add(CustomIntercetors());
+    if (kIsWeb) {
+      httpClientAdapter = BrowserHttpClientAdapter();
+    }
   }
 }
