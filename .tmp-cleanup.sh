7305f1638e8f93b57ceb90d651edd41863337347
Paulo Sena <paulo.sena@tce.am.gov.br>
2026-09-11 10:11:00 -0400

    chore: remove script temporario de commit

---
+++ .tmp-cleanup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+cd /home/paulosena/dev/oanse
+git add -A
+git commit -m "chore: remove script temporario de commit"
--- .tmp-commit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /home/paulosena/dev/oanse
-git add -A
-git commit -m "refactor: auto-import dos componentes PrimeVue via vite.config.ts"
